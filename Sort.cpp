3c804ce4546e10d251197f3d10d53acb37f5d6e5
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-05-19 15:25:56 +0530

    Sorting

---
+++ Sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+  ll n;
+  cin >> n;
+  ll a[n];
+  for (ll i = 0; i < n; i++)
+    cin >> a[i];
+  sort(a, a + n);
+  for (ll i = 0; i < n; i++)
+    cout << a[i] << endl;
+  return 0;
+}
